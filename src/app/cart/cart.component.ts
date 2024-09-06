dc8ddd5bd12aaaa6aaf59c9a4c92e66ab46a4ad4
Luca Chisari <75541078+Eddy7642@users.noreply.github.com>
2024-09-06 18:34:18 +0200

    Carrello

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrl: './product.component.scss'
 })
 export class ProductComponent implements OnInit {
-@Input() view: 'grid' | 'list' = 'grid';
+@Input() view: 'grid' | 'list' | 'currcartitem' | 'prevcartitem' = 'grid';
 constructor() {}
 
 ngOnInit(): void {
